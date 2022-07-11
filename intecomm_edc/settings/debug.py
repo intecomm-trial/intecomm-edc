189c47dd88a9b68a134e1de69d49f71c813047d5
frdrckaman <frdrckaman@gmail.com>
2022-07-11 14:30:59 +0300

    update site-names

--- intecomm_edc/settings/debug.py
+++ intecomm_edc/settings/debug.py
@@ -6,17 +6,31 @@ from .defaults import *  # noqa
 
 print(f"Settings file {__file__}")  # noqa
 
-SITE_ID = SiteID(default=30)
+SITE_ID = SiteID(default=201)
 EDC_SITES_UAT_DOMAIN = False
 DEBUG = True
 ALLOWED_HOSTS = [
     "localhost",
-    "mnazi-moja.tz.intecomm.clinicedc.org",
-    "mbagala.tz.intecomm.clinicedc.org",
-    "mwananyamala.tz.intecomm.clinicedc.org",
-    "hindu-mandal.tz.intecomm.clinicedc.org",
-    "temeke.tz.intecomm.clinicedc.org",
-    "amana.tz.intecomm.clinicedc.org",
+    "amana.uat.tz.intecomm.clinicedc.org"
+    "bagamoyo.uat.tz.intecomm.clinicedc.org"
+    "hindu-mandal.uat.tz.intecomm.clinicedc.org"
+    "kasangati.uat.ug.intecomm.clinicedc.org"
+    "kisarawe.uat.tz.intecomm.clinicedc.org"
+    "kisugu.uat.ug.intecomm.clinicedc.org"
+    "kiswa.uat.ug.intecomm.clinicedc.org"
+    "kyazanga.uat.ug.intecomm.clinicedc.org"
+    "mbagala.uat.tz.intecomm.clinicedc.org"
+    "mnazi.uat.tz.intecomm.clinicedc.org"
+    "mpigi.uat.ug.intecomm.clinicedc.org"
+    "mwananyamala.uat.tz.intecomm.clinicedc.org"
+    "namayumba.uat.ug.intecomm.clinicedc.org"
+    "namulonge.uat.ug.intecomm.clinicedc.org"
+    "ndejje.uat.ug.intecomm.clinicedc.org"
+    "rugambwa.uat.tz.intecomm.clinicedc.org"
+    "sekiwunga.uat.ug.intecomm.clinicedc.org"
+    "sinza.uat.tz.intecomm.clinicedc.org"
+    "temeke.uat.tz.intecomm.clinicedc.org"
+    "wakiso.uat.ug.intecomm.clinicedc.org"
 ]
 
 SECURE_SSL_REDIRECT = False
