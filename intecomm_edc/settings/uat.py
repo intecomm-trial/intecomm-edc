31d1f520c8f86c61c5409ccc3b01c1aebfa431b6
frdrckaman <frdrckaman@gmail.com>
2022-07-12 19:02:00 +0300

    updates sites domain names

--- intecomm_edc/settings/uat.py
+++ intecomm_edc/settings/uat.py
@@ -8,25 +8,25 @@ SITE_ID = SiteID(default=1)
 EDC_SITES_UAT_DOMAIN = True
 EDC_CONSENT_BYPASS_CONSENT_DATETIME_VALIDATION = True
 ALLOWED_HOSTS = [
-    "amana.uat.tz.intecomm.clinicedc.org"
-    "bagamoyo.uat.tz.intecomm.clinicedc.org"
-    "hindu-mandal.uat.tz.intecomm.clinicedc.org"
-    "kasangati.uat.ug.intecomm.clinicedc.org"
-    "kisarawe.uat.tz.intecomm.clinicedc.org"
-    "kisugu.uat.ug.intecomm.clinicedc.org"
-    "kiswa.uat.ug.intecomm.clinicedc.org"
-    "kyazanga.uat.ug.intecomm.clinicedc.org"
-    "mbagala.uat.tz.intecomm.clinicedc.org"
-    "mnazi.uat.tz.intecomm.clinicedc.org"
-    "mpigi.uat.ug.intecomm.clinicedc.org"
-    "mwananyamala.uat.tz.intecomm.clinicedc.org"
-    "namayumba.uat.ug.intecomm.clinicedc.org"
-    "namulonge.uat.ug.intecomm.clinicedc.org"
-    "ndejje.uat.ug.intecomm.clinicedc.org"
-    "rugambwa.uat.tz.intecomm.clinicedc.org"
-    "sekiwunga.uat.ug.intecomm.clinicedc.org"
-    "sinza.uat.tz.intecomm.clinicedc.org"
-    "temeke.uat.tz.intecomm.clinicedc.org"
-    "wakiso.uat.ug.intecomm.clinicedc.org"
+    "amana.uat.tz.intecomm.clinicedc.org",
+    "bagamoyo.uat.tz.intecomm.clinicedc.org",
+    "hindu-mandal.uat.tz.intecomm.clinicedc.org",
+    "kasangati.uat.ug.intecomm.clinicedc.org",
+    "kisarawe.uat.tz.intecomm.clinicedc.org",
+    "kisugu.uat.ug.intecomm.clinicedc.org",
+    "kiswa.uat.ug.intecomm.clinicedc.org",
+    "kyazanga.uat.ug.intecomm.clinicedc.org",
+    "mbagala.uat.tz.intecomm.clinicedc.org",
+    "mnazi.uat.tz.intecomm.clinicedc.org",
+    "mpigi.uat.ug.intecomm.clinicedc.org",
+    "mwananyamala.uat.tz.intecomm.clinicedc.org",
+    "namayumba.uat.ug.intecomm.clinicedc.org",
+    "namulonge.uat.ug.intecomm.clinicedc.org",
+    "ndejje.uat.ug.intecomm.clinicedc.org",
+    "rugambwa.uat.tz.intecomm.clinicedc.org",
+    "sekiwunga.uat.ug.intecomm.clinicedc.org",
+    "sinza.uat.tz.intecomm.clinicedc.org",
+    "temeke.uat.tz.intecomm.clinicedc.org",
+    "wakiso.uat.ug.intecomm.clinicedc.org",
     "localhost",
 ]
