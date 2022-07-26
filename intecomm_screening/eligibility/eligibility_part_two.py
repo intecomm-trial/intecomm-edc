6a3a02dce14e40977c45970a91824c1194b7f7c3
erikvw <ew2789@gmail.com>
2022-07-26 11:19:48 -0500

    part two eligibility

--- intecomm_screening/eligibility/eligibility_part_two.py
+++ intecomm_screening/eligibility/eligibility_part_two.py
@@ -1,10 +1,11 @@
+import decimal
 from typing import Any
 
 from edc_constants.constants import NO, TBD, YES
 from edc_reportable import MILLIMOLES_PER_LITER, ConversionNotHandled, convert_units
 from edc_screening.screening_eligibility import FC, ScreeningEligibility
 
-from ..constants import FBG_INCOMPLETE, FBG_LT_13, NOT_STUDY_DM, STUDY_DM
+from ..constants import FBG_INCOMPLETE
 
 
 class EligibilityPartTwo(ScreeningEligibility):
@@ -16,20 +17,31 @@ class EligibilityPartTwo(ScreeningEligibility):
         self.fbg_units = None
         self.fbg_value = None
         self.sys_blood_pressure_one = None
+        self.dia_blood_pressure_one = None
         self.unsuitable_agreed = None
         super().__init__(**kwargs)
 
     def assess_eligibility(self: Any) -> None:
         super().assess_eligibility()
-        if not self.fbg_category:
+        self.eligible = YES
+        if not self.fbg_value:
             self.eligible = TBD
             self.reasons_ineligible.update(fbg_incomplete=FBG_INCOMPLETE)
-        else:
-            if self.fbg_category == STUDY_DM:
-                self.eligible = YES
-            else:
-                self.eligible = NO
-                self.reasons_ineligible.update(fbg_low=FBG_LT_13)
+        elif self.converted_fbg_value <= 13:
+            self.eligible = NO
+            self.reasons_ineligible.update(fbg_low="fbg_low")
+        if not self.sys_blood_pressure_one:
+            self.eligible = TBD
+            self.reasons_ineligible.update(sys_incomplete="sys_incomplete")
+        elif self.sys_blood_pressure_one < 160:
+            self.eligible = NO
+            self.reasons_ineligible.update(systolic_low="systolic_low")
+        if not self.dia_blood_pressure_one:
+            self.eligible = TBD
+            self.reasons_ineligible.update(diastolic_incomplete="diastolic_incomplete")
+        elif self.dia_blood_pressure_one < 100:
+            self.eligible = NO
+            self.reasons_ineligible.update(diastolic_low="diastolic_low")
 
     def set_fld_attrs_on_model(self) -> None:
         self.model_obj.converted_fbg_value = self.converted_fbg_value
@@ -38,24 +50,12 @@ class EligibilityPartTwo(ScreeningEligibility):
         return {
             "fbg_units": FC(ignore_if_missing=True),
             "fbg_value": FC(ignore_if_missing=True),
+            "sys_blood_pressure_one": FC(ignore_if_missing=True),
+            "dia_blood_pressure_one": FC(ignore_if_missing=True),
         }
 
     @property
-    def fbg_category(self):
-        # FBG>13 mmol/L
-        value = self.converted_fbg_value
-        if not value:
-            fbg_category = None
-        elif value <= 13:
-            fbg_category = NOT_STUDY_DM
-        elif value > 13:
-            fbg_category = STUDY_DM
-        else:
-            raise ValueError("Invalid FBG value")
-        return fbg_category
-
-    @property
-    def converted_fbg_value(self):
+    def converted_fbg_value(self) -> float:
         try:
             value = convert_units(
                 self.fbg_value,
