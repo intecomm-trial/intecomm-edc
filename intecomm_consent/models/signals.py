8ec6b9cc8285c6a7c255803beafe46f9018c3264
erikvw <ew2789@gmail.com>
2023-01-24 05:47:51 -0600

    put on schedule on consent post-save, customize appt type

--- intecomm_consent/models/signals.py
+++ intecomm_consent/models/signals.py
@@ -1,6 +1,12 @@
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
 from django.core.exceptions import ValidationError
 from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
+from edc_randomization.utils import get_object_for_subject
+from edc_visit_schedule import site_visit_schedules
+from intecomm_rando.constants import CLINIC_CONTROL, COMM_INTERVENTION
 
 from intecomm_screening.models import SubjectScreening
 from intecomm_subject.models import SubjectVisit
@@ -29,14 +35,30 @@ def subject_consent_on_post_save(sender, instance, raw, created, **kwargs):
             subject_screening.patient_log.save_base(
                 update_fields=["subject_identifier", "consent_datetime"]
             )
-            # # put subject on schedule
-            # _, schedule = site_visit_schedules.get_by_onschedule_model(
-            #     "intecomm_prn.onschedulebaseline"
-            # )
-            # schedule.put_on_schedule(
-            #     subject_identifier=instance.subject_identifier,
-            #     onschedule_datetime=instance.consent_datetime,
-            # )
+        elif instance.group_identifier:
+            # put subject on schedule once in a group
+            rando_obj = get_object_for_subject(
+                instance.group_identifier, "default", identifier_fld="group_identifier"
+            )
+            if rando_obj.assignment in [COMM_INTERVENTION, CLINIC_CONTROL]:
+                model_name = (
+                    "intecomm_prn.onschedulecomm"
+                    if rando_obj.assignment == COMM_INTERVENTION
+                    else "intecomm_prn.onscheduleinte"
+                )
+                _, schedule = site_visit_schedules.get_by_onschedule_model(model_name)
+                allocated_datetime = datetime(
+                    rando_obj.allocated_datetime.year,
+                    rando_obj.allocated_datetime.month,
+                    rando_obj.allocated_datetime.day,
+                    rando_obj.allocated_datetime.minute,
+                    rando_obj.allocated_datetime.second,
+                    tzinfo=ZoneInfo("UTC"),
+                )
+                schedule.put_on_schedule(
+                    subject_identifier=instance.subject_identifier,
+                    onschedule_datetime=allocated_datetime,
+                )
 
 
 @receiver(
