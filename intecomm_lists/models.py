ec6ce8b426c660660b0a32bf2826328c1f6febff
erikvw <ew2789@gmail.com>
2023-03-28 08:26:52 -0500

    add missing list data

--- intecomm_lists/models.py
+++ intecomm_lists/models.py
@@ -161,3 +161,15 @@ class LocationTypes(ListModelMixin):
     class Meta(ListModelMixin.Meta):
         verbose_name = "Community-care location type"
         verbose_name_plural = "Community-care location types"
+
+
+class RxModificationReasons(ListModelMixin):
+    class Meta(ListModelMixin.Meta):
+        verbose_name = "Treatment Modification Reasons"
+        verbose_name_plural = "Treatment Modification Reasons"
+
+
+class RxModifications(ListModelMixin):
+    class Meta(ListModelMixin.Meta):
+        verbose_name = "Treatment Modifications"
+        verbose_name_plural = "Treatment Modifications"
