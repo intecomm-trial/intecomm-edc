7f73aa0cf4691ca8bcc2bd98b141e284aa422dc9
erikvw <ew2789@gmail.com>
2023-04-03 12:25:05 -0500

    add pre-delete signals to protect consent and subject screening

--- intecomm_consent/models/__init__.py
+++ intecomm_consent/models/__init__.py
@@ -1,3 +1,3 @@
-from .signals import subject_consent_on_post_delete, subject_consent_on_post_save
+from .signals import subject_consent_on_post_save, subject_consent_on_pre_delete
 from .subject_consent import SubjectConsent
 from .subject_reconsent import SubjectReconsent
