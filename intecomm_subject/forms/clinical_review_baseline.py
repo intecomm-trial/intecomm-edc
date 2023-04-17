c22ed446a787af49693c6136795e8cf9944c7d61
Jonathan Willitts <JonathanWillitts@users.noreply.github.com>
2023-04-17 20:42:11 +0100

    Switch to ClinicalReviewBaselineFormValidator from intecomm-form-validators

--- intecomm_subject/forms/clinical_review_baseline.py
+++ intecomm_subject/forms/clinical_review_baseline.py
@@ -1,16 +1,8 @@
 from django import forms
-from edc_crf.crf_form_validator_mixins import CrfFormValidatorMixin
 from edc_crf.modelform_mixins import CrfModelFormMixin
-from edc_dx_review.form_validator_mixins import ClinicalReviewBaselineFormValidatorMixin
-from edc_form_validators import FormValidator
+from intecomm_form_validators.subject import ClinicalReviewBaselineFormValidator
 
 from ..models import ClinicalReviewBaseline
-
-
-class ClinicalReviewBaselineFormValidator(
-    ClinicalReviewBaselineFormValidatorMixin, CrfFormValidatorMixin, FormValidator
-):
-    pass
 
 
 class ClinicalReviewBaselineForm(CrfModelFormMixin, forms.ModelForm):
