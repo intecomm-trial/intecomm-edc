0dd95d3894eb3acb916e8f1c0c2759c19cfd0095
Jonathan Willitts <JonathanWillitts@users.noreply.github.com>
2023-04-20 21:03:54 +0100

    Add "clinic_days","club_days", and "purchased_days" to model admin (#548)

--- intecomm_subject/admin/drug_refill_hiv_admin.py
+++ intecomm_subject/admin/drug_refill_hiv_admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django_audit_fields import audit_fieldset_tuple
+from edc_crf.admin import crf_status_fieldset_tuple
 from edc_model_admin.mixins import TabularInlineMixin
 from edc_rx.modeladmin_mixins import DrugRefillAdminMixin, DrugSupplyInlineMixin
 
@@ -17,5 +19,36 @@ class DrugSupplyHivInline(DrugSupplyInlineMixin, TabularInlineMixin, admin.Tabul
 class DrugRefillHivAdmin(DrugRefillAdminMixin, CrfModelAdmin):
     form = DrugRefillHivForm
     autocomplete_fields = ["rx"]
+
+    fieldsets = (
+        (None, {"fields": ("subject_visit", "report_datetime")}),
+        (
+            "Refill Information",
+            {
+                "fields": (
+                    "rx",
+                    "rx_other",
+                    "rx_modified",
+                    "modifications",
+                    "modifications_other",
+                    "modifications_reason",
+                    "modifications_reason_other",
+                )
+            },
+        ),
+        (
+            "Drug Supply: HIV",
+            {
+                "fields": (
+                    "return_in_days",
+                    "clinic_days",
+                    "club_days",
+                    "purchased_days",
+                )
+            },
+        ),
+        crf_status_fieldset_tuple,
+        audit_fieldset_tuple,
+    )
 
     filter_horizontal = ["modifications", "modifications_reason"]
