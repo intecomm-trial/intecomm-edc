41e129f56fdc583411efeb754d42dedd463f29aa
erikvw <ew2789@gmail.com>
2023-05-03 21:44:58 -0500

    add group name and link to group changelist to subject dashboard

--- intecomm_dashboard/views/subject/dashboard/dashboard_view.py
+++ intecomm_dashboard/views/subject/dashboard/dashboard_view.py
@@ -1,4 +1,7 @@
+from django.urls import reverse
 from edc_subject_dashboard.views import SubjectDashboardView
+
+from intecomm_group.models import PatientGroup
 
 
 class DashboardView(SubjectDashboardView):
@@ -9,5 +12,16 @@ class DashboardView(SubjectDashboardView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context.update(subject_listboard_url="screen_group_url")
+        group = PatientGroup.objects.get(
+            group_identifier=context.get("consent").object.group_identifier
+        )
+        url = reverse("intecomm_group_admin:intecomm_group_patientgroup_changelist")
+        url = f"{url}?q={group.name}"
+
+        context.update(
+            subject_listboard_url="screen_group_url",
+            group_identifier=group.group_identifier,
+            group_name=group.name,
+            patient_group_url=url,
+        )
         return context
