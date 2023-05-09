9f2858198c0f2c026169d9122006b6e863db2638
erikvw <ew2789@gmail.com>
2023-05-08 22:07:38 -0500

    change label for missed apptointment

--- intecomm_dashboard/views/subject/dashboard/dashboard_view.py
+++ intecomm_dashboard/views/subject/dashboard/dashboard_view.py
@@ -2,6 +2,9 @@ from django.urls import reverse
 from edc_subject_dashboard.views import SubjectDashboardView
 
 from intecomm_group.models import PatientGroup
+from intecomm_subject.constants import NOT_SCHEDULED_FOR_FACILITY
+
+from ....model_wrappers import AppointmentModelWrapper
 
 
 class DashboardView(SubjectDashboardView):
@@ -9,6 +12,8 @@ class DashboardView(SubjectDashboardView):
     navbar_selected_item = "consented_subject"
     visit_model = "intecomm_subject.subjectvisit"
     history_button_label = "Audit"
+
+    appointment_model_wrapper_cls = AppointmentModelWrapper
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -23,5 +28,6 @@ class DashboardView(SubjectDashboardView):
             group_identifier=group.group_identifier,
             group_name=group.name,
             patient_group_url=url,
+            NOT_SCHEDULED_FOR_FACILITY=NOT_SCHEDULED_FOR_FACILITY,
         )
         return context
