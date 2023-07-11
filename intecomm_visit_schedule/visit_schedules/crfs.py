7f37105f0ec7ce58379a59202e879f5f8bff87ad
erikvw <ew2789@gmail.com>
2023-07-11 10:46:13 -0500

    add family history to baseline

--- intecomm_visit_schedule/visit_schedules/crfs.py
+++ intecomm_visit_schedule/visit_schedules/crfs.py
@@ -27,6 +27,7 @@ crfs_d1 = FormsCollection(
     Crf(show_order=155, model="intecomm_subject.drugrefillhiv", required=False),
     Crf(show_order=160, model="intecomm_subject.otherbaselinedata"),
     Crf(show_order=165, model="intecomm_subject.complicationsbaseline"),
+    Crf(show_order=220, model="intecomm_subject.familyhistory"),
     Crf(show_order=240, model="intecomm_subject.icecapa"),
     Crf(show_order=245, model="intecomm_subject.eq5d3l"),
     Crf(show_order=300, model="intecomm_subject.healtheconomicshouseholdhead"),
