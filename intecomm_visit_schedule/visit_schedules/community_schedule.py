4f6e52149e98417bc6d3cd6fe2a2ba71bd281f34
erikvw <ew2789@gmail.com>
2023-08-10 21:25:06 -0500

    fix missing unscheduled crfs/requisitions for baseline visit, cleanup code

--- intecomm_visit_schedule/visit_schedules/community_schedule.py
+++ intecomm_visit_schedule/visit_schedules/community_schedule.py
@@ -1,13 +1,13 @@
 from edc_visit_schedule import Schedule
 
-from ..constants import INTE_SCHEDULE
+from ..constants import COMMUNITY_SCHEDULE
 from .visits import get_followup_visit, visit00, visit12
 
-inte_schedule = Schedule(
-    name=INTE_SCHEDULE,
-    verbose_name="facility-based integrated care",
-    onschedule_model="intecomm_prn.onscheduleinte",
-    offschedule_model="intecomm_prn.offscheduleinte",
+community_schedule = Schedule(
+    name=COMMUNITY_SCHEDULE,
+    verbose_name="Community-based integrated care",
+    onschedule_model="intecomm_prn.onschedulecomm",
+    offschedule_model="intecomm_prn.offschedulecomm",
     consent_model="intecomm_consent.subjectconsent",
     appointment_model="edc_appointment.appointment",
 )
@@ -18,4 +18,4 @@ for month in range(1, 12):
 visits.append(visit12)
 
 for visit in visits:
-    inte_schedule.add_visit(visit=visit)
+    community_schedule.add_visit(visit=visit)
