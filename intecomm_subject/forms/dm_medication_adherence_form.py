b5131bcca82b3fa47b3f23794220ec2963b597b3
erikvw <ew2789@gmail.com>
2023-08-22 19:59:09 -0500

    change labels

--- intecomm_subject/forms/dm_medication_adherence_form.py
+++ intecomm_subject/forms/dm_medication_adherence_form.py
@@ -10,9 +10,18 @@ class DmMedicationAdherenceForm(CrfModelFormMixin, forms.ModelForm):
     form_validator_cls = DmMedicationAdherenceFormValidator
 
     visual_score_slider = forms.CharField(
-        label="Visual Score", widget=SliderWidget(attrs={"min": 0, "max": 100})
+        label="Visual score for your diabetes medication",
+        widget=SliderWidget(attrs={"min": 0, "max": 100}),
     )
 
     class Meta:
         model = DmMedicationAdherence
         fields = "__all__"
+        labels = {
+            "last_missed_pill": (
+                "When was the last time you missed taking your diabetes medication?"
+            ),
+            "missed_pill_reason": (
+                "If your diabetes medication was missed, select a reason or reasons"
+            ),
+        }
--- intecomm_subject/forms/hiv_medication_adherence_form.py
+++ intecomm_subject/forms/hiv_medication_adherence_form.py
@@ -10,9 +10,18 @@ class HivMedicationAdherenceForm(CrfModelFormMixin, forms.ModelForm):
     form_validator_cls = HivMedicationAdherenceFormValidator
 
     visual_score_slider = forms.CharField(
-        label="Visual Score", widget=SliderWidget(attrs={"min": 0, "max": 100})
+        label="Visual score for your HIV medication",
+        widget=SliderWidget(attrs={"min": 0, "max": 100}),
     )
 
     class Meta:
         model = HivMedicationAdherence
         fields = "__all__"
+        labels = {
+            "last_missed_pill": (
+                "When was the last time you missed taking your HIV medication?"
+            ),
+            "missed_pill_reason": (
+                "If your HIV medication was not taken, select a reason or reasons"
+            ),
+        }
