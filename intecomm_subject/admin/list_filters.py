2c50d5dc78f514349f6c15323a46e4e05a072b6f
erikvw <ew2789@gmail.com>
2024-02-01 20:23:12 -0600

    vl resulted list filter hiv review

--- intecomm_subject/admin/list_filters.py
+++ intecomm_subject/admin/list_filters.py
@@ -1,5 +1,7 @@
 from django.contrib.admin import SimpleListFilter
-from django.db.models import Count
+from django.db.models import Count, QuerySet
+from edc_constants.choices import YES_NO_PENDING_NA
+from edc_constants.constants import NO, NOT_APPLICABLE, PENDING, YES
 from edc_sites.site import sites
 
 from intecomm_group.models import PatientGroup
@@ -38,3 +40,25 @@ class PatientGroupListFilter(SimpleListFilter):
             ]
             queryset = queryset.filter(subject_identifier__in=subject_identifiers)
         return queryset
+
+
+class VlStatusListFilter(SimpleListFilter):
+    title = "VL Resulted"
+
+    parameter_name = "vl_resulted"
+    field_name = "has_vl"
+
+    def lookups(self, request, model_admin) -> tuple:
+        return YES_NO_PENDING_NA
+
+    def queryset(self, request, queryset) -> QuerySet | None:
+        qs = None
+        if self.value() == YES:
+            qs = queryset.filter(has_vl=YES)
+        if self.value() == NO:
+            qs = queryset.filter(has_vl=NO)
+        if self.value() == PENDING:
+            qs = queryset.filter(has_vl=PENDING)
+        if self.value() == NOT_APPLICABLE:
+            qs = queryset.filter(has_vl=NOT_APPLICABLE)
+        return qs
--- intecomm_subject/admin/modeladmin_mixins.py
+++ intecomm_subject/admin/modeladmin_mixins.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING, Type
+
 from django.contrib import admin
 from django.utils.html import format_html
 from django_audit_fields import audit_fieldset_tuple
@@ -11,6 +13,9 @@ from edc_model_admin.mixins import ModelAdminProtectPiiMixin
 from edc_sites.admin import SiteModelAdminMixin
 
 from intecomm_subject.choices import MISSED_PILLS
+
+if TYPE_CHECKING:
+    from django.contrib.admin import SimpleListFilter
 
 medication_adherence_description = """
 <H5><B><font color="orange">Interviewer to read</font></B></H5>
@@ -34,7 +39,7 @@ class CrfModelAdmin(
     ordering = ("site__id", "report_datetime")
 
     # TODO: remove with Django > 4.2.5
-    def get_list_filter(self, request) -> tuple[str, ...]:
+    def get_list_filter(self, request) -> tuple[str | Type[SimpleListFilter], ...]:
         list_filter = super().get_list_filter(request)
         self.list_filter = list_filter
         return list_filter
